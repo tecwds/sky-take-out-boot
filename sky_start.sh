b51358b8f3783154c511216c89174c5eae982bf1
wpan <tecwds@163.com>
2024-02-09 16:28:15 +0800

    初始化...

---
+++ sky_start.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# 1. 前置变量
+# sky_take_out 默认路径
+SKY_HOME=/~/docker/workspace/sky_take_out
+
+if [ $# -ne 1 ]; then
+  echo "用法：程序名称 [SKY_HOME路径]"
+  echo "将使用默认参数: $SKY_HOME"
+else
+  SKY_HOME=$1
+  # echo "SKY_HOME: $SKY_HOME"
+
+fi
+
+echo "SKY_HOME = $SKY_HOME"
+
+echo "设置环境变量"
+export SKY_HOME=$SKY_HOME
+
+
+# 2. 复制文件
+cp -ur ./mysql "$SKY_HOME"
+cp -ur ./nginx "$SKY_HOME"
+cp -ur ./redis "$SKY_HOME"
+
+# 2.1 解压文件
+unzip "$SKY_HOME"/nginx/html.zip
+mv "$SKY_HOME"/nginx/html.zip /tmp
+
+# 3. 检查
+docker-compose config -q
+docker-compose up -d
